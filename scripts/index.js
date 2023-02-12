80dfe845b167a7f6e6cbab3a1fe74e4ae7fdda5c
MS3012-91 <mariasheynik@gmail.com>
2023-02-12 18:35:50 +0100

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+// let a=1;
+// while (a<=10) {
+//     console.log (a);
+//     a++
+// }
+// let b=10;
+// while (b >0) {
+//     console.log (b);
+//     b--;
+// }
+
+let count = 0;
+let add;
+while (add <=10){
+console.log (count+add);
+add = add++;
+}
+
+let consta = 4;
+let num = 1;
+while (
+num <= 10) {
+result=consta*num;
+console.log (result);
+num++
+}
+
+
