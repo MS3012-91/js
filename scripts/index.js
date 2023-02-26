24e7b094c254c7903d3b197f1a00973aafc019cc
MS3012-91 <mariasheynik@gmail.com>
2023-02-26 21:25:31 +0100

    homework

--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,50 @@
-// 1. Определите переменные со значениями 100, 2.5, false, "Hello, java script!" и выведите их в консоль
-let a = 100,
-    b = 2.5,
-    c = false,
-    d = "Hello, java script!";
-console.log(a, b, c, d);
 
-// // 2. Создайте переменные greeting со значением'Hi, ' и userName. Спросите имя пользователя 
-// // и занесите данное значение в переменную userName. С помощью диалога выведите сообщение с приветствием. 
-// // Например, 'Hi, Vasya!'.
-let greeting='Hi, ';
-let userName=prompt ('Whats your name?')
-alert (greeting + userName);
-
-// // 3. Выведите числа от 10 до 50, которые кратны 5
-let x=10;
-while (x<50){
-    x=x+5;
-    console.log (x);
-}
-
-// // 4. Спросите у пользователя номер месяца (это будет число из интервала от 1 до 12) . 
-// // Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
-let season = prompt ('Enter a number of month')
-    if (season == 1 & season == 2 & season == 12) alert ('winter')
-else if (season >=3 & season<= 5) alert ('spring')
-else if (season >=6 & season<= 9) alert ('summer')
-else alert ('autmn');
-
-
-// 5. Подсчитайте сумму четных чисел от 1 до 100
-let firstNumber = 2;
+//1. Сумма отрицательных элементов массива
+const numbers = [18, 0, 23, 2, 4, -15, 0, 24, -3];
 let sum=0;
-while (firstNumber<=100) {
-    sum=sum+firstNumber;
-    firstNumber = firstNumber+2;
-}
+for (i=0; i<numbers.length; i++) {
+if (numbers[i]<0) {sum=sum+numbers[i]} else sum;}
 console.log (sum);
 
-
-
-// Дополнительные:
-// 6. Вывести знак * столько раз сколько задаст пользователь
-let symbol = '*'
-let num = prompt ('Enter number of stars');
-while (num>0){
-    console.log(symbol);
-    num=num-1;
+//2. Замените все отрицательные элементы массива на ноль
+for (i=0; i<numbers.length; i++) {
+    if (numbers[i]<0) {numbers[i]=0} else numbers[i];
 }
+console.log (numbers);
 
-/*7. делить число 1000 на 2 до тех пор, пока не получится число меньше 50.
- Какое число получилось? сколько раз делили?*/
-let count = 1000;
-let constant = 2;
-let result = count/constant;
-while (result>=50) {
-    result=result/constant;
-    console.log (result);
+//Найдите произведение элементов массива , которые меньше 10, но положительные
+let summary=0
+for (i=0; i-numbers.length; i++) {
+    if (numbers[i]<10 && numbers[i]>0)
+ {summary = summary+numbers[i]} 
+    else summary;
 }
+console.log ('summary =', summary)
 
+//4. Выведите индексы нулевых элементов массива (которые равны нулю), сохраняя результат в отдельный массив
+let a=[], j=0;
+for (i=0; i<numbers.length; i++) {
+    if (numbers[i]==0) {
+        a[j]=i; j++
+    }
+    else a[j]
+}
+console.log (a)
 
-// 8. вывести таблицу значений для функции y = 5x + 2 для значения х от -1 до 1 с шагом 0.1. Вывод вида:
-// x = -1 , y = -3
-// x = -0.9, y = -2.5
-// ....
-// x = 1, y = 7
-let variable;
-let res;
-for (let variable = -1; variable < 1; variable=variable+0.1) {
-    res=5*variable+2;
-        console.log ('x=', variable, 'y=', res);}   
+//5. Вернуть первые N элемента массива
+let l=[], p=0, n=5;
+for (i=0; i<numbers.length; i++) {
+if (i<n) {l[p]=numbers[i]; p++}
+else l[p]
+}
+console.log (l);
+
+//6* найдите максимальный элемент массива
+//const numbers = [18, 0, 23, 2, 4, -15, 0, 24, -3];
+let max=0;
+for (i=0; i<numbers.length; i++) {
+    if (numbers[i]>max) {
+        max=numbers[i]
+    }
+}
+console.log (max)
